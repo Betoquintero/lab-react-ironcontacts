af44a8bb5319a89a46487d56a39ff586dd579e4d
Alberto <aquintero88@gmail.com>
2022-07-27 17:48:26 +0200

    it 1 & 2 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import contactData from "./contacts.json";
 
 function App() {
+
+  const arrContact = [...contactData].slice(0,5)
+
+  const [contacts, setContacts] = useState(arrContact) 
+
+  const restOfContacts = contactData.slice(5)
+  
+  const handleRandom = () => {
+    let randomContact = restOfContacts[Math.floor(Math.random()*restOfContacts.length )]
+    const newArr = [...contacts]
+    newArr.push(randomContact)
+    setContacts(newArr)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className="action-btn" onClick={handleRandom}>Add random</button>
+    <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+      </table>
+        {contacts.map(elem => {
+        return (
+                  <tr key={elem.id} className="table">
+                    <td><img width="40px" src={elem.pictureUrl} /></td>
+                    <td>{elem.name}</td>
+                    <td>{elem.popularity.toFixed(2)}</td>
+                  </tr>
+        )        
+      })}
     </div>
   );
 }
