aea26f667413bc450372cfe5eabb3688ac63c502
Alberto <aquintero88@gmail.com>
2022-07-28 09:53:32 +0200

    missing CSS

--- src/App.js
+++ src/App.js
@@ -17,23 +17,50 @@ function App() {
     setContacts(newArr)
   }
 
+  const handlePopularity = () => {
+    let orderPopularity = [...contacts].sort((a,b) => b.popularity - a.popularity)
+    setContacts(orderPopularity)
+  }
+
+  const handleNameSort = () => {
+    let orderedNames = [...contacts].sort((a,b) => a.name.localeCompare(b.name))
+    setContacts(orderedNames)
+  }
+
+  const handleDelete = contactId => {
+    let deletingContact = contacts.filter(elem =>{
+      return elem.id !== contactId
+    })
+    setContacts(deletingContact)
+
+  }
+
 
   return (
     <div className="App">
     <button className="action-btn" onClick={handleRandom}>Add random</button>
-    <table>
-        <tr>
+    <button className="action-btn" onClick={handleNameSort}>Sort by name</button>
+    <button className="action-btn" onClick={handlePopularity}>Sort Popularity</button>    
+    <table className="table">
+        <tr className="tableHeadings">
           <th>Picture</th>
           <th>Name</th>
           <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
         </tr>
       </table>
         {contacts.map(elem => {
+          const oscarWin=elem.wonOscar && <span>🏆</span>
+          const emmyWin=elem.wonEmmy && <span>🏆</span>
         return (
-                  <tr key={elem.id} className="table">
-                    <td><img width="40px" src={elem.pictureUrl} /></td>
-                    <td>{elem.name}</td>
-                    <td>{elem.popularity.toFixed(2)}</td>
+                  <tr key={elem.id} className="contacts">
+                    <td className="contactImage"><img  width="50px" src={elem.pictureUrl} /></td>
+                    <td className="contactName">{elem.name}</td>
+                    <td className="contactPopularity">{elem.popularity.toFixed(2)}</td>
+                    <td className="contactOscar">{oscarWin}</td>
+                    <td className="contactEmmy">{emmyWin}</td>
+                    <td><button className="action-btn" onClick={() => handleDelete(elem.id)}>Delete</button></td>
                   </tr>
         )        
       })}
